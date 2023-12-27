f9ab0ad7363732cca3340c250f84bdb988e6d242
rohan-nittur <rohan.nittur@gmail.com>
2023-12-27 17:58:32 +0000

    Add test form handle route

--- back_end/app.py
+++ back_end/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    text = request.form['text']
+    return f"Message received: {text}"
+
 
 if __name__ == '__main__':
     app.run()
